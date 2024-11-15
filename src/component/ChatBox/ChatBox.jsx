3f92d52f39be40192d536fc5c4d16665b0392dc1
Empire688682 <asehindej@gmail.com>
2024-11-15 21:46:32 +0100

    chatbox

--- src/component/ChatBox/ChatBox.jsx
+++ src/component/ChatBox/ChatBox.jsx
@@ -4,8 +4,11 @@ import style from './ChatBox.module.css';
 const ChatBox = () => {
   return (
     <div className={style.chat_box}>
-      <h1>CHAT BOX</h1>
-      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Atque et ipsum necessitatibus magni earum perferendis voluptate mollitia maiores, amet culpa natus voluptatum, incidunt iure? Quidem laudantium eius assumenda aut quibusdam.</p>
+      <div className={style.chat_box_Con}>
+        <div className={style.header}>
+          
+        </div>
+      </div>
     </div>
   )
 }
