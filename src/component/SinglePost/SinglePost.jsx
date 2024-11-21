7d13c48585eca89fff3455039440e392a4c5c909
Empire688682 <asehindej@gmail.com>
2024-11-21 23:22:13 +0100

    share

--- src/component/SinglePost/SinglePost.jsx
+++ src/component/SinglePost/SinglePost.jsx
@@ -13,9 +13,11 @@ const SinglePost = () => {
             <div className={style.single_post_Con}>
                 <div className={style.header}>
                     <div className={style.header_left}>
-                        <Image src='/profile_alison.png' width={80} height={80} alt='Img' sizes='100%' />
-                        <p className={style.user_name}>Simba Rossie</p>
-                        <span>Nov 19 at 4:25 PM</span>
+                        <Image className={style.user_img} src='/profile_alison.png' width={50} height={50} alt='Img' sizes='100%' />
+                        <div className={style.user_name_time}>
+                            <p className={style.user_name}>Simba Rossie</p>
+                            <span>Nov 19 at 4:25 PM</span>
+                        </div>
                     </div>
                     <div className={style.header_right}>
                         <BsThreeDots className={style.icon} />
@@ -28,13 +30,15 @@ const SinglePost = () => {
                 </p>
                 <div className={style.post_images}>
                     <div className={style.img_big_Con}>
-                        <Image src='/profile_alison.png' fill alt='Img' sizes='100%' />
+                        <Image className={style.post_img} src='/pic1.png' fill alt='Img' sizes='100%' />
+                    </div>
+                    <div className={style.small_img_Cons}>
+                    <div className={style.img_small_Con}>
+                        <Image className={style.post_img} src='/img1.jpg' fill alt='Img' sizes='100%' />
                     </div>
                     <div className={style.img_small_Con}>
-                        <Image src='/profile_alison.png' fill alt='Img' sizes='100%' />
+                        <Image className={style.post_img} src='/img2.jpg' fill alt='Img' sizes='100%' />
                     </div>
-                    <div className={style.img_small_Con}>
-                        <Image src='/profile_alison.png' fill alt='Img' sizes='100%' />
                     </div>
                 </div>
                 <div className={style.post_reaction_header}>
@@ -52,21 +56,21 @@ const SinglePost = () => {
                             <TiArrowForward className={style.icon} />
                         </div>
                     </div>
-                    <div className={style.post_reaction_Con}>
+                </div>
+                <div className={style.post_reaction_Con}>
                         <div className={style.like}>
                             <AiOutlineLike className={style.icon} />
                             <p>Like</p>
                         </div>
                         <div className={style.comment}>
-                        <BiMessage className={style.icon} />
-                         <p>Comment</p>
+                            <BiMessage className={style.icon} />
+                            <p>Comment</p>
                         </div>
                         <div className={style.share}>
-                        <p>Share</p>
-                        <TiArrowForward className={style.icon} />
+                            <p>Share</p>
+                            <TiArrowForward className={style.icon} />
                         </div>
                     </div>
-                </div>
             </div>
         </div>
     )
