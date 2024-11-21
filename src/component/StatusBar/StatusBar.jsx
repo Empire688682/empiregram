21d372a28aeedb22eaea7538fc8db885b2a8d0c7
Empire688682 <asehindej@gmail.com>
2024-11-21 15:25:20 +0100

    bug

--- src/component/StatusBar/StatusBar.jsx
+++ src/component/StatusBar/StatusBar.jsx
@@ -1,21 +1,21 @@
 import React from 'react';
-import style from'./StatusBar.mudule.css';
+import style from './StatusBar.mudule.css';
 import Image from 'next/image';
 
 const StatusBar = () => {
   return (
     <div className={style.status_bar}>
-     <div className={style.status_bar_Con}>
-      <div className={style.user}>
-        <Image className={style.user_img} src='/avatar_icon.png' fill alt='IMG' sizes='100%'/>
-        <div className={style.user_plus}>
-          <p>+</p>
-          <span>Create story</span>
+      <div className={style.status_bar_Con}>
+        <div className={style.user}>
+          <Image className={style.user_img} src='/avatar_icon.png' fill alt='IMG' sizes='100%' />
+          <div className={style.user_plus}>
+            <p>+</p>
+            <span>Create story</span>
+          </div>
         </div>
       </div>
-     </div>
     </div>
-  )
+  );
 }
 
-export default StatusBar
+export default StatusBar;
