baa94a29969af48ebc309a4bdef023dab9dd396b
Empire688682 <asehindej@gmail.com>
2024-11-21 18:45:07 +0100

    swipper

--- src/component/StatusBar/StatusBar.jsx
+++ src/component/StatusBar/StatusBar.jsx
@@ -13,6 +13,17 @@ const StatusBar = () => {
             <span>Create story</span>
           </div>
         </div>
+       {
+        Array(10).fill('').map((item, id) => (
+          <div className={style.friends} key={id}>
+          <Image className={style.friends_img} src='/profile_enrique.png' fill alt='IMG' sizes='100%' />
+          <div className={style.friends_post_Con}>
+            <Image className={style.secondary_friends_img} src='/img1.jpg' width={50} height={50} alt='IMG' sizes='100%' />
+          </div>
+          <p className={style.friends_name}>Simba Rossie</p>
+        </div>
+        ))
+       }
       </div>
     </div>
   );
