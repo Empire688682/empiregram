afb3761ccb85330d942121012b88fdd7eb1a094f
Empire688682 <asehindej@gmail.com>
2024-11-22 15:13:50 +0100

    video

--- src/app/video/page.jsx
+++ src/app/video/page.jsx
@@ -1,10 +1,22 @@
 import React from 'react';
 import style from './video.module.css';
+import VideoLeftBar from '@/component/VideoLeftBar/VideoLeftBar';
+import Video from '@/component/Video/Video';
 
 const page = () => {
   return (
     <div className={style.video}>
-      
+      <div className={style.video_nav}>
+        NAVBAR HERE
+      </div>
+      <div className={style.video_Con}>
+        <div className={style.left_bar}>
+        <VideoLeftBar/>
+        </div>
+        <div className={style.video_box}>
+        <Video/>
+        </div>
+      </div>
     </div>
   )
 }
--- src/component/VideoLeftBar/VideoLeftBar.jsx
+++ src/component/VideoLeftBar/VideoLeftBar.jsx
@@ -1,10 +1,50 @@
 import React from 'react';
 import style from './VideoLeftBar.module.css';
+import { IoIosAddCircle } from "react-icons/io";
+import { BsThreeDotsVertical } from "react-icons/bs";
+import { IoSearchOutline } from "react-icons/io5";
+import ChatMenu from '../ChatMenu/ChatMenu';
+import Image from 'next/image';
 
 const VideoLeftBar = () => {
   return (
-    <div className={style.video_leftBar}>
-      
+    <div className={style.video_left_bar}>
+      <div className={style.video_left_bar_Con}>
+        <div className={style.header_fixed}>
+          <div className={style.header}>
+            <h2>Video</h2>
+            <IoIosAddCircle className={style.icon} />
+            <BsThreeDotsVertical className={style.icon} />
+          </div>
+          <div className={style.search_bar}>
+            <label htmlFor="search">
+              <IoSearchOutline className={style.search_bar_icon} />
+            </label>
+            <input type="text" id='search' placeholder='Search' name='search' required />
+          </div>
+        </div>
+        <div className={style.users_list}>
+        {
+          Array(100).fill('').map((item, id) => (
+            <div className={style.users} key={id}>
+              <div className={style.left_side}>
+                <div className={style.img_Con}>
+                  <Image src='/profile_marco.png' fill alt='User' />
+                </div>
+                <div className={style.name_msg}>
+                  <p>Jayempire</p>
+                  <span>Hello world</span>
+                </div>
+              </div>
+              <div className={style.right_side}>
+                <p>Today</p>
+                <span>1</span>
+              </div>
+            </div>
+          ))
+        }
+        </div>
+      </div>
     </div>
   )
 }
