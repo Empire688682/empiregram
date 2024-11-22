9a081cd135afee39f4c54b8ad741f4c5760b4b17
Empire688682 <asehindej@gmail.com>
2024-11-22 13:34:14 +0100

    video

---
+++ src/component/VideoLeftBar/VideoLeftBar.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './VideoLeftBar.module.css';
+
+const VideoLeftBar = () => {
+  return (
+    <div className={style.video_leftBar}>
+      
+    </div>
+  )
+}
+
+export default VideoLeftBar
