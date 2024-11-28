b2b8211255bad62dd8b87b8fc63bdb53d7cd3d67
Empire688682 <asehindej@gmail.com>
2024-11-28 10:55:55 +0100

    home

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import style from './settimgs.module.css';
+import Settings from '@/component/Settings/Settings';
 
 const page = () => {
   return (
     <div className={style.settings}>
-      <h1>Settings</h1>
+      <Settings/>
     </div>
   )
 }
