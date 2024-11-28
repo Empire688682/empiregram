34d7fb6ce28f5981eabb1a984a8b40b509d2782b
Empire688682 <asehindej@gmail.com>
2024-11-28 13:15:07 +0100

    menubar

--- src/app/settings/page.jsx
+++ src/app/settings/page.jsx
@@ -1,11 +1,37 @@
+';use client'
 import React from 'react';
 import style from './settimgs.module.css';
 import Settings from '@/component/Settings/Settings';
-import SettingsLeftBar from '@/component/SettingsLeftBar/SettingsLeftBar';
+import Image from 'next/image';
+import { IoMdHome } from "react-icons/io";
+import { MdOutlineVideoLibrary } from "react-icons/md";
+import { FaUserGroup } from "react-icons/fa6";
+import { IoNotifications } from "react-icons/io5";
+import { FaFacebookMessenger } from "react-icons/fa";
+import Link from 'next/link';
 
 const page = () => {
   return (
     <div className={style.settings}>
+        <div className={style.settings_nav}>
+        <Image className={style.logo} src='/empiregram-logo.webp' width={50} height={50} sizes='100%' alt='Logo' />
+        <div className={style.menu_icons}>
+          <Link href='/chat' style={{textDecoration:'none'}}>
+          <IoMdHome className={style.icon} />
+          </Link>
+          <Link href='/video' style={{textDecoration:'none'}}>
+          <MdOutlineVideoLibrary className={style.icon} />
+          </Link>
+          <Link href='/chat' style={{textDecoration:'none'}}>
+          <FaUserGroup className={style.icon} />
+          </Link>
+        </div>
+        <div className={style.user_menus}>
+        <Image className={style.user_img} src='/avatar_icon.png' width={50} height={50} sizes='100%' alt='Logo' />
+        <IoNotifications className={style.icon}/>
+        <FaFacebookMessenger className={style.icon}/>
+        </div>
+      </div>
       <Settings/>
     </div>
   )
--- src/component/Settings/Settings.jsx
+++ src/component/Settings/Settings.jsx
@@ -1,36 +1,17 @@
 'use client'
 import React from 'react';
 import style from './Settings.module.css';
-import Link from 'next/link';
-import { MdOutlineVideoLibrary } from "react-icons/md";
-import { FaUserGroup } from "react-icons/fa6";
-import { MdNotifications } from "react-icons/md";
-import { IoMdHome } from "react-icons/io";
 import SettingsLeftBar from '../SettingsLeftBar/SettingsLeftBar';
 
 const Settings = () => {
     return (
         <div className={style.settings}>
             <div className={style.settings_Con}>
-                <div className={style.header}>
-                    <Link href='/chat' style={{ textDecoration: 'none' }}>
-                        <IoMdHome className={style.icon} />
-                    </Link>
-                    <Link href='/video' style={{ textDecoration: 'none' }}>
-                        <MdOutlineVideoLibrary className={style.icon} />
-                    </Link>
-                    <Link href='/chat' style={{ textDecoration: 'none' }}>
-                        <FaUserGroup className={style.icon} />
-                    </Link>
-                    <MdNotifications className={style.icon} />
+                <div className={style.SettingsLeftBar}>
+                    <SettingsLeftBar />
                 </div>
-                <div className={style.settings_main_Con}>
-                    <div className={style.SettingsLeftBar}>
-                        <SettingsLeftBar />
-                    </div>
-                    <div className={style.Settings_Content}>
-                        
-                    </div>
+                <div className={style.Settings_Content}>
+
                 </div>
             </div>
         </div>
