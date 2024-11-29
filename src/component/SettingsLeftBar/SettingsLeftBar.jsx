9196986e520ffce2159e917b1aa0d6335552e378
Empire688682 <asehindej@gmail.com>
2024-11-29 19:36:51 +0100

    settinfs

--- src/component/SettingsLeftBar/SettingsLeftBar.jsx
+++ src/component/SettingsLeftBar/SettingsLeftBar.jsx
@@ -1,9 +1,14 @@
 'use client;'
 import React from 'react';
 import style from "./SettingsLeftBar.module.css";
-import { IoSettingsOutline } from "react-icons/io5";
-import { MdVideoLibrary } from "react-icons/md";
-import { FaPhotoVideo  } from "react-icons/fa";
+import { IoNotifications } from "react-icons/io5";
+import { FaTags } from "react-icons/fa";
+import { CgProfile } from "react-icons/cg";
+import { MdPostAdd } from "react-icons/md";
+import { GiThreeFriends } from "react-icons/gi";
+import { LiaBirthdayCakeSolid } from "react-icons/lia";
+import { FaPhotoVideo } from "react-icons/fa";
+
 const SettingsLeftBar = () => {
   return (
     <div className={style.settings_left_bar}>
@@ -13,8 +18,32 @@ const SettingsLeftBar = () => {
         </div>
         <div className={style.settings_cart_Con}>
         <div className={style.settings_cart}>
-          <MdVideoLibrary className={style.icons} />
+          <IoNotifications className={style.icons} />
           <p>Notification settings</p>
+        </div>
+        <div className={style.settings_cart}>
+          <FaTags className={style.icons} />
+          <p>Tags</p>
+        </div>
+        <div className={style.settings_cart}>
+          <CgProfile className={style.icons} />
+          <p>Profile details</p>
+        </div>
+        <div className={style.settings_cart}>
+          <MdPostAdd className={style.icons} />
+          <p>Post</p>
+        </div>
+        <div className={style.settings_cart}>
+          <GiThreeFriends className={style.icons} />
+          <p>Friends Requests</p>
+        </div>
+        <div className={style.settings_cart}>
+          <LiaBirthdayCakeSolid className={style.icons} />
+          <p>Birthdays</p>
+        </div>
+        <div className={style.settings_cart}>
+          <FaPhotoVideo className={style.icons} />
+          <p>Birthdays</p>
         </div>
         </div>
       </div>
