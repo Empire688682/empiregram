b66470dcdccd5fac828c26621d818ad01d172ad2
Empire688682 <asehindej@gmail.com>
2024-11-30 15:40:31 +0100

    message

--- src/component/Settings/Settings.jsx
+++ src/component/Settings/Settings.jsx
@@ -2,6 +2,7 @@
 import React, { useState } from 'react';
 import style from './Settings.module.css';
 import SettingsLeftBar from '../SettingsLeftBar/SettingsLeftBar';
+import { FaRegMessage } from "react-icons/fa6";
 
 const Settings = () => {
     const [mode, setMode] = useState("on");
@@ -22,6 +23,42 @@ const Settings = () => {
                                 <div className={style.cart_left}>
                                     <h3>Who can see your post</h3>
                                     <p>Your default audience is set to Public. This will be your audience for future posts, but you can always change it for a specific post.</p>
+                                </div>
+                                <div className={style.cart_right}>
+                                    <select name="viewer" id="">
+                                        <option value="Public">Public</option>
+                                        <option value="Private">Private</option>
+                                        <option value="Friends">Friends</option>
+                                        <option value="Only me">Only me</option>
+                                    </select>
+                                </div>
+                            </div>
+                            <div className={style.secondary_cart}>
+                                <div className={style.cart_left}>
+                                    <h3>Allow visual search on your posts</h3>
+                                    <p>Visual search finds other content on Facebook related to your post, to help people discover more of what they are interested in. This is your default, but you can always change it for specific posts. </p>
+                                </div>
+                                <div className={style.cart_right}>
+                                    <div className={style.mode_right}>
+                                        <span onClick={handleMode} className={mode === "on" ? `${style.mode_motor} ${style.dark}`:`${style.mode_motor}`}></span>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={style.container}>
+                        <h2>Notifications settings</h2>
+                        <p>Empiregram may still send you important notifications about your account and content outside of your preferred notification settings.</p>
+                        <h3>What Notification You Recieve</h3>
+                        <div className={style.cart}>
+                            <div className={style.secondary_cart}>
+                                <div className={style.cart_left}>
+                                    <FaRegMessage className={style.cart_left_icon}/>
+                                    <div className={style.cart_left_text}>
+                                      <p>Comments</p>
+                                      <span>Push, Email, SMS</span>
+                                    </div>
                                 </div>
                                 <div className={style.cart_right}>
                                     <select name="viewer" id="">
