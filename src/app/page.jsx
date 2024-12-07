2742dc1fc54f6c0b5ed04d360e40af9979044a5d
Empire688682 <asehindej@gmail.com>
2024-12-08 00:40:34 +0100

    pge

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -14,7 +14,7 @@ const page = () => {
             <div className={style.col_Con}>
                 <div className={style.left_col}>
                     <div className={style.img_Con}>
-                    <Image src={IMG} alt="Img" fill />
+                    <Image src="/empiregram-logo.webp" alt="Img" fill />
                     </div>
                 </div>
                 <div className={style.right_col}>
