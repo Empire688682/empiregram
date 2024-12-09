5d89de7e40c4e727af57745a647c0d96b18c870b
Empire688682 <asehindej@gmail.com>
2024-12-09 19:47:34 +0100

    groups

--- src/component/YourGroups/YourGroup.jsx
+++ src/component/YourGroups/YourGroup.jsx
@@ -34,20 +34,26 @@ const YourGroups = () => {
             <div className={style.group_you_are_con}>
                 <h2>All groups you have joined (200) </h2>
                 <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolorum magni sunt similique unde quibusdam cupiditate modi autem ut architecto! Sapiente, repudiandae facilis dicta neque laborum ipsam quisquam vitae atque natus.</p>
-                <div className={style.group_you_are_cart}>
-                    <div className={style.group_you_are_header}>
-                        <div className={style.group_you_are_img_con}>
-                            <Image className={style.group_you_are_img} src='/img1.jpg' alt='IMG' fill />
+                <div className={style.group_you_are_cart_con}>
+                {
+                    Array.from({length: 2}).map((_, id) => (
+                        <div className={style.group_you_are_cart} key={id}>
+                            <div className={style.group_you_are_header}>
+                                <div className={style.group_you_are_img_con}>
+                                    <Image className={style.group_you_are_img} src='/img1.jpg' alt='IMG' fill />
+                                </div>
+                                <div className={style.group_you_are_text_all}>
+                                    <p>Amazon kindle group</p>
+                                    <span>Last request 2 years ago</span>
+                                </div>
+                            </div>
+                            <div className={style.group_you_are_footer}>
+                                <button>Update responses</button>
+                                <BsThreeDots className={style.icon} />
+                            </div>
                         </div>
-                        <div className={style.group_you_are_text_all}>
-                            <p>Amazon kindle group</p>
-                            <span>Last request 2 years ago</span>
-                        </div>
-                    </div>
-                    <div className={style.group_you_are_footer}>
-                        <button>View group</button>
-                        <BsThreeDots className={style.icon} />
-                    </div>
+                    ))
+                }
                 </div>
             </div>
         </div>
