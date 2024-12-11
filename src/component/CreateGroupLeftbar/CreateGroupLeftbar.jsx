507114f73f4d73976ee50d5b067e4f3a3dd304ae
Empire688682 <asehindej@gmail.com>
2024-12-11 09:00:59 +0100

    new folder

---
+++ src/component/CreateGroupLeftbar/CreateGroupLeftbar.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './CreateGroupLeftbar.module.css';
+
+const CreateGroupLeftbar = () => {
+  return (
+    <div className={style.create_group_leftbar}>
+        <div className={style.create_group_leftbar_header}>
+
+        </div>
+    </div>
+  )
+}
+
+export default CreateGroupLeftbar
