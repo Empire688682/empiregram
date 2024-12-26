d87c0508e286332829f98e9b6cb788cb264e99fa
Empire688682 <asehindej@gmail.com>
2024-12-26 14:14:40 +0100

    new folder

---
+++ src/component/GroupEvent/GroupEvent.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from './GroupEvent.module.css';
+
+const GroupEvent = () => {
+  return (
+    <div className={style.groupEvent}>
+      
+    </div>
+  )
+}
+
+export default GroupEvent
