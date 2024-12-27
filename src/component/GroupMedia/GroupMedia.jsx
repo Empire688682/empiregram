79c4518ef66f91411424e0878c580f2c251aeae2
Empire688682 <asehindej@gmail.com>
2024-12-27 21:56:58 +0100

    new folder

---
+++ src/component/GroupMedia/GroupMedia.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from './GroupMedia.module.css';
+
+const GroupMedia = () => {
+  return (
+    <div className={style.groupMediaContainer}>
+        <div className={style.groupMedia}>
+
+        </div>
+    </div>
+  )
+}
+
+export default GroupMedia
