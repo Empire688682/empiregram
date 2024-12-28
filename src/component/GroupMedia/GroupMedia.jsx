68c8b5fe91b9a8bb063bcfaa76d7207b5ad234c1
Empire688682 <asehindej@gmail.com>
2024-12-28 22:50:32 +0100

    new folder

--- src/component/GroupMedia/GroupMedia.jsx
+++ src/component/GroupMedia/GroupMedia.jsx
@@ -1,12 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 import style from './GroupMedia.module.css';
+import { FaPlus } from "react-icons/fa6";
 
 const GroupMedia = () => {
+  const [displayMenu, setDisplayMenu] = useState("photos");
   return (
     <div className={style.groupMediaContainer}>
-        <div className={style.groupMedia}>
-
+      <div className={style.groupMedia}>
+        <div className={style.header}>
+          <h2>Media</h2>
+          <ul>
+            <li><FaPlus /> <span>Create album</span></li>
+            <li>Add photos/video</li>
+          </ul>
         </div>
+        <div className={style.displayMenu}>
+          <li>Photos</li>
+          <li>Videos</li>
+          <li>Albums</li>
+        </div>
+      </div>
     </div>
   )
 }
