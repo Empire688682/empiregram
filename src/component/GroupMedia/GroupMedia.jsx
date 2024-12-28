71150e22d864e2b4d2114b276734d4bcafbcaba9
Empire688682 <asehindej@gmail.com>
2024-12-28 23:41:55 +0100

    photos

--- src/component/GroupMedia/GroupMedia.jsx
+++ src/component/GroupMedia/GroupMedia.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import style from './GroupMedia.module.css';
 import { FaPlus } from "react-icons/fa6";
+import MediaPhotos from './MediaPhotos/MediaPhotos';
 
 const GroupMedia = () => {
   const [displayMenu, setDisplayMenu] = useState("photos");
@@ -10,7 +11,7 @@ const GroupMedia = () => {
         <div className={style.header}>
           <h2>Media</h2>
           <ul>
-            <li><FaPlus /> <span>Create album</span></li>
+            <li><FaPlus /> Create album</li>
             <li>Add photos/video</li>
           </ul>
         </div>
@@ -18,6 +19,11 @@ const GroupMedia = () => {
           <li onClick={()=> setDisplayMenu("photos")}>Photos</li>
           <li  onClick={()=> setDisplayMenu("videos")}>Videos</li>
           <li  onClick={()=> setDisplayMenu("albums")}>Albums</li>
+        </div>
+        <div className={style.displayContent}>
+          {
+            displayMenu === "photos" && <MediaPhotos/>
+          }
         </div>
       </div>
     </div>
---
+++ src/component/GroupMedia/MediaPhotos/MediaPhotos.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import style from './MediaPhotos.module.css';
+import Image from "next/image";
+
+const MediaPhotos = () => {
+  return (
+    <div className={style.mediaPhotos}>
+      {
+        Array.from({length:35}).map((_, id)=>(
+           <Image key={id} alt='Img' src="/profile_richard.png" className={style.img} width={80} height={80} sizes='100%'/>
+        ))
+      }
+    </div>
+  )
+}
+
+export default MediaPhotos
