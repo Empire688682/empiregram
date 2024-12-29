a1ae4153878ffff17fed7ecdbf5538c074d4cc0e
Empire688682 <asehindej@gmail.com>
2024-12-29 21:21:03 +0100

    video

---
+++ src/component/GroupMedia/MideaAlbum/MideaAlbum.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import style from "./MideaAlbum.module.css";
+
+const MideaAlbum = () => {
+  return (
+    <div className={style.mideaAlbumCon}>
+      <div className={style.mideaAlbum}>
+        <p>No result</p>
+      </div>
+    </div>
+  )
+}
+
+export default MideaAlbum
