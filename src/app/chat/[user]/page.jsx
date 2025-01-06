864a16c90bf5e11d71216975827f282000d7cc79
Empire688682 <asehindej@gmail.com>
2025-01-06 23:32:30 +0100

    header

--- src/app/chat/[user]/page.jsx
+++ src/app/chat/[user]/page.jsx
@@ -3,35 +3,35 @@ import React from 'react';
 import style from "./user.module.css";
 import { useParams } from 'next/navigation';
 import Image from 'next/image';
+import { FaVideo } from "react-icons/fa";
+import { IoCall } from "react-icons/io5";
+import { FiMinus } from "react-icons/fi";
+import { LiaTimesSolid } from "react-icons/lia";
 
 const Page = () => {
     const user = useParams();
-    console.log("user",user);
-  return (
-    <div className={style.user}>
-      <div className={style.user_Con}>
-        <div className={style.user_header}>
-          <div className={style.user_img_con}>
-            <Image className={style.user_img} src='/img1.jpg' alt='IMG' fill sizes='100%' />
-          </div>
-          <div className={style.user_text_all}>
-            <p>{user}</p>
-            <span>Active now</span>
-          </div>
+    console.log("user", user);
+    return (
+        <div className={style.user}>
+            <div className={style.userCon}>
+                <div className={style.userHeader}>
+                    <div className={style.headerImgCon}>
+                        <Image className={style.userImg} src='/img1.jpg' alt='IMG' width={50} height={50} sizes='100%' />
+                        <div className={style.userText_all}>
+                            <p>{user.user}</p>
+                            <span>Active now</span>
+                        </div>
+                    </div>
+                    <div className={style.headerIconsCon}>
+                        <FiMinus className={style.Icon} />
+                        <LiaTimesSolid className={style.Icon} />
+                        <IoCall className={style.Icon} />
+                        <FaVideo className={style.Icon} />
+                    </div>
+                </div>
+            </div>
         </div>
-        <div className={style.user_footer}>
-          <div className={style.user_footer_item}>
-            <i className="fa-solid fa-video"></i>
-            <p>Video Call</p>
-          </div>
-          <div className={style.user_footer_item}>
-            <i className="fa-solid fa-phone"></i>
-            <p>Audio Call</p>
-          </div>
-        </div>
-      </div>
-    </div>
-  )
+    )
 }
 
 export default Page
