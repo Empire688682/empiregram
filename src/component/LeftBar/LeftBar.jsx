1c05b4dd8c0d44c3712ccfd28f625f44ca4e198a
Empire688682 <asehindej@gmail.com>
2025-01-07 17:35:36 +0100

    onclick

--- src/component/LeftBar/LeftBar.jsx
+++ src/component/LeftBar/LeftBar.jsx
@@ -1,17 +1,22 @@
-'use client'
+'use client';
 import React from 'react';
 import style from './LeftBar.module.css';
-import { IoIosAddCircle } from "react-icons/io";
-import { BsThreeDotsVertical } from "react-icons/bs";
-import { IoSearchOutline } from "react-icons/io5";
+import { IoIosAddCircle } from 'react-icons/io';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import { IoSearchOutline } from 'react-icons/io5';
 import ChatMenu from '../ChatMenu/ChatMenu';
 import Image from 'next/image';
 import { useGlobalContext } from '../Context';
 import UserChatDisplay from '../UserChatDisplay/UserChatDisplay';
 
 const LeftBar = () => {
-  const {friends, loading} = useGlobalContext();
-  const [openUser, setOpenUser] = React.useState(true);
+  const { friends, loading } = useGlobalContext();
+  const [selectedUser, setSelectedUser] = React.useState(null);
+
+  const handleUserClick = (user) => {
+    setSelectedUser(user);
+  };
+
   return (
     <div className={style.left_bar}>
       <div className={style.left_bar_Con}>
@@ -25,43 +30,54 @@ const LeftBar = () => {
             <label htmlFor="search">
               <IoSearchOutline className={style.search_bar_icon} />
             </label>
-            <input type="text" id='search' placeholder='Search' name='search' required />
+            <input
+              type="text"
+              id="search"
+              placeholder="Search"
+              name="search"
+              required
+            />
           </div>
           <ChatMenu />
         </div>
-        {
-          loading ? <div className={style.loading}>
+        {loading ? (
+          <div className={style.loading}>
             <h2>Loading...</h2>
           </div>
-          :
+        ) : (
           <div className={style.users_list}>
-        {
-          friends.map((friend) => (
-            <div onClick={()=>setOpenUser(true)} className={style.users} key={friend.id}>
-              {
-                openUser ? <UserChatDisplay user={friend} /> : null
-              }
-              <div className={style.left_side}>
-                <div className={style.img_Con}>
-                  <Image src={friend.user_Img} fill alt='User' sizes='100%' />
+            {friends.map((friend) => (
+              <div
+                onClick={() => handleUserClick(friend)}
+                className={style.users}
+                key={friend.id}
+              >
+                <div className={style.left_side}>
+                  <div className={style.img_Con}>
+                    <Image
+                      src={friend.user_Img}
+                      fill
+                      alt="User"
+                      sizes="100%"
+                    />
+                  </div>
+                  <div className={style.name_msg}>
+                    <p>{friend.user_Name}</p>
+                    <span>{friend.currentMessage}</span>
+                  </div>
                 </div>
-                <div className={style.name_msg}>
-                  <p>{friend.user_Name}</p>
-                  <span>{friend.currentMessage}</span>
+                <div className={style.right_side}>
+                  <p>Today</p>
+                  <span>1</span>
                 </div>
               </div>
-              <div className={style.right_side}>
-                <p>Today</p>
-                <span>1</span>
-              </div>
-            </div>
-          ))
-        }
-        </div>
-        }
+            ))}
+          </div>
+        )}
       </div>
+      {selectedUser && <UserChatDisplay user={selectedUser} />}
     </div>
-  )
-}
+  );
+};
 
-export default LeftBar
+export default LeftBar;
