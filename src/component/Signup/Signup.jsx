412509a70a5fe462bfaabd15c5c6db77ead1fd4a
Empire688682 <asehindej@gmail.com>
2025-01-23 13:44:15 +0100

    js

--- src/component/Signup/Signup.jsx
+++ src/component/Signup/Signup.jsx
@@ -37,7 +37,7 @@ const Signup = () => {
             <input
               type="password"
               name="Rpwd"
-              placeholder="Confirm Password"
+              placeholder="Comfirm password"
               required
             />
           ) : (
