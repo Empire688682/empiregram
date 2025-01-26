2be34c2eca6e0cccf5c7b239d3618f8752afc8c5
Empire688682 <asehindej@gmail.com>
2025-01-26 16:14:26 +0100

    login

--- src/app/api/auth/login/route.js
+++ src/app/api/auth/login/route.js
@@ -1,11 +1,46 @@
-const { NextResponse } = require("next/server");
+import { NextResponse } from "next/server";
+import { usersModel } from "../../models/user";
+import jwt from "jsonwebtoken";
+import bcrypt from 'bcryptjs';
+import dotenv from 'dotenv';
+dotenv.config();
 
 
-const loginUser = (req) =>{
+const loginUser = async (req) =>{
+   if(req.method === "POST"){
     try {
-        
+        const reqBody = await req.json();
+        const {username, password} = reqBody;
+
+        const user = usersModel.findOne({username});
+
+        if(!user){
+            return NextResponse.json({success:false, message: "User not found"}, {status:400})
+        };
+
+        const isPasswordMatch = bcrypt.compare(password, user.password);
+        if(!isPasswordMatch){
+            return NextResponse.json({success:false, message: "Incorrect password"}, {status:400})
+        }
+
+        const token = jwt.sign({id:user._id}, "juwon");
+        const res = NextResponse.json({success:true, message:"Log in successfully", user: newUser}, {status:200});
+        res.cookies.set("EmpireGToken", token,{
+            httpOnly: true,
+            secure: process.env.NODE_ENV === "production" ? true : false,
+            maxAge: 2 * 24 * 60 * 60, // 2 days
+            sameSite: "lax",
+            path: "/",
+        });
+        return res;
+
     } catch (error) {
         console.log("Error:", error);
-        return NextResponse.json({success:false, message:"Logout user error"}, {status:500})
+        return NextResponse.json({success:false, message:"Unable to login user"}, {status:500})
     }
+   }
+}
+
+export async function POST(){
+    return loginUser(req);
 }
