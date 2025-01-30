46d0d39271918b90f2a6fe4d7cbd362108c605d9
Empire688682 <asehindej@gmail.com>
2025-01-30 03:47:32 +0100

    new folder

--- src/app/[username]/page.jsx
+++ src/app/[username]/page.jsx
@@ -1,11 +1,14 @@
 import React from 'react';
 import style from "./Username.module.css";
+import UserData from '@/component/UserData/UserData';
+import MenuBar from '@/component/MenuBar/MenuBar';
 
 const page = () => {
   return (
     <div className={style.username}>
-      <div>
-        <h1>Hello world</h1>
+      <MenuBar/>
+      <div className={style.userData}>
+        <UserData/>
       </div>
     </div>
   )
---
+++ src/component/UserData/UserData.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import style from "./UserData.module.css";
+
+const UserData = () => {
+  return (
+    <div className={style.userData}>
+      <h1>User Data</h1>
+    </div>
+  )
+}
+
+export default UserData
