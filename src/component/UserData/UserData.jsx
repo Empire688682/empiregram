4c6de2748ee192a26566394c445a6679f0f42a41
Empire688682 <asehindej@gmail.com>
2025-01-30 06:00:20 +0100

    css

--- src/component/UserData/UserData.jsx
+++ src/component/UserData/UserData.jsx
@@ -13,38 +13,40 @@ const UserData = () => {
                 <Image style={{ objectFit: "cover" }} src="/pic3.png" alt="" fill sizes="100%" />
                 <button aria-label="Edit cover photo" className={style.editCoverBtn}> <IoIosCamera /> <span>Edit cover photo</span></button>
             </div>
-            <div className={style.userProfile}>
-                <div className={style.userProfileImg}>
-                    <Image className={style.userProfileImg} style={{ objectFit: "cover" }} src="/profile_alison.png" alt="" fill sizes="100%" />
-                    <IoIosCamera className={style.userProfileImgIcon} />
+            <div className={style.userProfileCon}>
+                <div className={style.userProfile}>
+                    <div className={style.userProfileImg}>
+                        <Image className={style.userProfileImg} style={{ objectFit: "cover" }} src="/profile_alison.png" alt="" fill sizes="100%" />
+                        <IoIosCamera className={style.userProfileImgIcon} />
+                    </div>
+                    <div className={style.userProfileNamesNfriends}>
+                        <div className={style.userProfileName}>
+                            <h2>Juwon Asehinde</h2>
+                            <span>-Jayempire</span>
+                            <span>505 friends</span>
+                        </div>
+                        <div className={style.userProfileFriends}>
+                            {
+                                Array.from({ length: 10 }).map((_, id) => (
+                                    <Image style={{ borderRadius: "50%", cursor: "pointer" }} key={id} className={style.userfriendsImg} src="/profile_enrique.png" alt="" width={40} height={40} sizes="100%" />
+                                ))
+                            }
+                        </div>
+                    </div>
                 </div>
-            </div>
-            <div className={style.userProfileNamesNfriends}>
-                <div className={style.userProfileName}>
-                    <h2>Juwon Asehinde</h2>
-                    <span>-Jayempire</span>
-                    <span>505 friends</span>
+                <div className={style.userProfileEdit}>
+                    <button>
+                        <FaPlus />
+                        <span>Add to story</span>
+                    </button>
+                    <button>
+                        <MdEdit />
+                        <span>Edit profile</span>
+                    </button>
+                    <button>
+                        <FaAngleDown />
+                    </button>
                 </div>
-                <div className={style.userProfileFriends}>
-                    {
-                        Array.from({ length: 10 }).map((_, id) => (
-                            <Image key={id} className={style.userfriendsImg} src="/profile_enrique.png" alt="" width={100} height={100} sizes="100%" />
-                        ))
-                    }
-                </div>
-            </div>
-            <div className={style.userProfileEdit}>
-                <button>
-                    <FaPlus />
-                    <span>Add to story</span>
-                </button>
-                <button>
-                    <MdEdit />
-                    <span>Add to story</span>
-                </button>
-                <button>
-                    <FaAngleDown />
-                </button>
             </div>
         </div>
     )
