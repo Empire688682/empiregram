e9af2d7b689a77fff70c9bebdbc20bcb842fbc88
Empire688682 <asehindej@gmail.com>
2025-01-31 21:52:26 +0100

    bravo

--- src/component/UserProfilePost/GridView.jsx
+++ src/component/UserProfilePost/GridView.jsx
@@ -9,32 +9,36 @@ import { MdGroups } from "react-icons/md";
 const GridView = () => {
   return (
     <div className={style.gridViewCon}>
-      <div className={style.gridView}>
-        <div className={style.imgCon}>
-          <Image
-          src="/pic1.png"
-          alt="Img"
-          fill
-          sizes='100%'
-          style={{objectFit:"cover"}}
-          />
-        </div>
-        <div className={style.gridViewContent}>
-        <Image
-          className={style.gridViewContentImg}
-          src="/pic1.png"
-          alt="Img"
-          width={40}
-          height={40}
-          sizes='100%'
-          />
-          <div className={style.gridViewContentText}>
-            <p>Juwon Asehinde</p>
-            <span>June 27, 2023</span>
-            <MdGroups />
+      {
+        Array.from({ length: 12 }).map((_, id) => (
+          <div className={style.gridView} key={id}>
+            <div className={style.imgCon}>
+              <Image
+                src="/pic1.png"
+                alt="Img"
+                fill
+                sizes='100%'
+                style={{ objectFit: "cover" }}
+              />
+            </div>
+            <div className={style.gridViewContent}>
+              <Image
+                className={style.gridViewContentImg}
+                src="/pic1.png"
+                alt="Img"
+                width={40}
+                height={40}
+                sizes='100%'
+              />
+              <div className={style.gridViewContentText}>
+                <p>Juwon Asehinde</p>
+                <span>June 27, 2023</span>
+                <MdGroups />
+              </div>
+            </div>
           </div>
-        </div>
-      </div>
+        ))
+      }
     </div>
   )
 }
