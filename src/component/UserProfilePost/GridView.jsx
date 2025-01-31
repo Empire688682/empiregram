4d2be8febc72e3708af2ba5eb45a58a69b719477
Empire688682 <asehindej@gmail.com>
2025-01-31 21:59:20 +0100

    grid

--- src/component/UserProfilePost/GridView.jsx
+++ src/component/UserProfilePost/GridView.jsx
@@ -2,19 +2,21 @@
 import React from 'react';
 import style from "./UserProfilePost.module.css";
 import Image from "next/image";
-import { FaUserFriends } from "react-icons/fa";
 import { FaLock } from "react-icons/fa";
 import { MdGroups } from "react-icons/md";
+import { MdOutlinePublic } from "react-icons/md";
+import { LiaUserFriendsSolid } from "react-icons/lia";
+import { allPosts } from "@/component/data";
 
 const GridView = () => {
   return (
     <div className={style.gridViewCon}>
       {
-        Array.from({ length: 12 }).map((_, id) => (
-          <div className={style.gridView} key={id}>
+        allPosts.map((post) => (
+          <div className={style.gridView} key={post.id}>
             <div className={style.imgCon}>
               <Image
-                src="/pic1.png"
+                src={post.big_Img}
                 alt="Img"
                 fill
                 sizes='100%'
@@ -24,16 +26,20 @@ const GridView = () => {
             <div className={style.gridViewContent}>
               <Image
                 className={style.gridViewContentImg}
-                src="/pic1.png"
+                src={post.user_Img}
                 alt="Img"
                 width={40}
                 height={40}
                 sizes='100%'
               />
               <div className={style.gridViewContentText}>
-                <p>Juwon Asehinde</p>
-                <span>June 27, 2023</span>
-                <MdGroups />
+                <p>{post.user_Name}</p>
+                <span>{post.date}</span>
+                {post.privacy === "public" ? (
+                    <MdOutlinePublic className={style.icon} />
+                  ) : (
+                    <LiaUserFriendsSolid className={style.icon} />
+                  )}
               </div>
             </div>
           </div>
