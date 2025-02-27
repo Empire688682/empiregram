2355dcc343124e5d6419251f46328dca98c5a671
Empire688682 <asehindej@gmail.com>
2025-02-27 07:32:11 +0100

    post

--- src/component/StatusBar/StatusBar.jsx
+++ src/component/StatusBar/StatusBar.jsx
@@ -8,21 +8,6 @@ const StatusBar = ({ handleGetPostId }) => {
   return (
     <div className={style.status_bar}>
       <div className={style.status_bar_Con}>
-      <div className={style.user_Con}>
-      <div className={style.user}>
-          <Image
-            className={style.user_img}
-            src="/avatar_icon.png"
-            fill
-            alt="IMG"
-            sizes="100%"
-          />
-          <div className={style.user_plus}>
-            <p>+</p>
-            <span>Create story</span>
-          </div>
-        </div>
-      </div>
 
         {//loading && (
           //<div className={style.loading}>
@@ -31,10 +16,10 @@ const StatusBar = ({ handleGetPostId }) => {
           // )
         }
 
-        <div className={style.post_slider_Con}>
+        <div className={style.status_Con}>
           {
             Array.from({ length: 100 }).map((_, i) => (
-              <div className={style.freiend} key={i}>
+              <div className={style.status} key={i}>
                 <Image
                   className={style.user_img}
                   src="/avatar_icon.png"
