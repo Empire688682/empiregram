e68d29d82bd1ff754bf59b869334691378d413d5
Empire688682 <asehindej@gmail.com>
2025-03-01 09:46:47 +0100

    statusbar

--- src/component/StatusBar/StatusBar.jsx
+++ src/component/StatusBar/StatusBar.jsx
@@ -5,33 +5,57 @@ import { useGlobalContext } from "../Context";
 
 const StatusBar = ({ handleGetPostId }) => {
   const { posts, loading } = useGlobalContext();
+  console.log("posts", posts);
   return (
     <div className={style.status_bar}>
       <div className={style.status_bar_Con}>
 
-        {//loading && (
-          //<div className={style.loading}>
-          //  <h2>Loading...</h2>
-          //</div>
-          // )
-        }
+       {
+         loading && (
+          <div className={style.loading}>
+            <h2>Loading...</h2>
+          </div>
+           )
+       }
 
         <div className={style.status_Con}>
           {
-            Array.from({ length: 100 }).map((_, i) => (
+            posts.map((post, i) => (
+              //{
+              //User Status bar
+             //   <div className={style.status} key={i}>
+             //   <Image
+             //     className={style.user_img}
+             //     src="/avatar_icon.png"
+              //    fill
+              //    alt="IMG"
+              //    sizes="100%"
+              //  />
+              //  <div className={style.user_plus}>
+              //    <p>+</p>
+                //  <span>Create story</span>
+               // </div>
+             // </div>
+             // }
               <div className={style.status} key={i}>
+              <Image
+                className={style.user_img}
+                src="/avatar_icon.png"
+                fill
+                alt="IMG"
+                sizes="100%"
+              />
+              <div className={style.friends_small_img_container}>
                 <Image
-                  className={style.user_img}
-                  src="/avatar_icon.png"
-                  fill
-                  alt="IMG"
-                  sizes="100%"
+                className={style.friends_small_img}
+                src="/avatar_icon.png"
+                width={45}
+                alt="IMG"
+                sizes="100%"
+                height={45}
                 />
-                <div className={style.user_plus}>
-                  <p>+</p>
-                  <span>Create story</span>
-                </div>
               </div>
+            </div>
             ))
           }
         </div>
