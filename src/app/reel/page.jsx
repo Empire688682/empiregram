0a3a6a0918383c7a11994fcdbcdc34e331760ee8
Empire688682 <asehindej@gmail.com>
2025-03-15 06:34:38 +0100

    bug free

--- src/app/reel/page.jsx
+++ src/app/reel/page.jsx
@@ -1,21 +1,18 @@
-"use client"
-import React, { useEffect, useState } from 'react';
-import style from './Reel.module.css'
-import ReelComp from '@/component/ReelComp/ReelComp';
-import { useRouter, useSearchParams } from 'next/navigation';
-import { useGlobalContext } from '@/component/Context';
+"use client";
+import React, { useEffect, useState, Suspense } from "react";
+import style from "./Reel.module.css";
+import ReelComp from "@/component/ReelComp/ReelComp";
+import { useRouter, useSearchParams } from "next/navigation";
+import { useGlobalContext } from "@/component/Context";
 
-const Page = () => {
-  const {
-    fetchReels,
-    reels
-  } = useGlobalContext();
+const ReelPage = () => {
+  const { fetchReels, reels } = useGlobalContext();
   const [index, setIndex] = useState(0);
   const [videoId, setVideoId] = useState("");
   const router = useRouter();
   const searchParams = useSearchParams();
 
-  if(videoId){
+  if (videoId) {
     console.log("videoId:", videoId.id);
   }
 
@@ -28,7 +25,7 @@ const Page = () => {
   useEffect(() => {
     if (reels.length > 0) {
       const newVideoId = reels[index]?.id || "";
-      
+
       // Update URL with the new video ID (without reloading)
       if (newVideoId) {
         const params = new URLSearchParams(searchParams.toString());
@@ -39,36 +36,37 @@ const Page = () => {
   }, [index, reels, router, searchParams]);
 
   const nextVideo = () => {
-    setIndex((prevIndex) => 
-      (prevIndex === reels.length - 1 ? prevIndex 
-        : prevIndex + 1
-      ));
-
+    setIndex((prevIndex) =>
+      prevIndex === reels.length - 1 ? prevIndex : prevIndex + 1
+    );
   };
-  
 
-  const nextBackVideo = () =>{
-    setIndex((prevIndex)=>{
-      if(prevIndex === 0){
-        return 0
-      }
-      else{
-        return prevIndex - 1
-      }
-    });
-  }
+  const nextBackVideo = () => {
+    setIndex((prevIndex) => (prevIndex === 0 ? 0 : prevIndex - 1));
+  };
 
-  useEffect(()=>{
+  useEffect(() => {
     fetchReels();
-  },[fetchReels]);
-  
+  }, [fetchReels]);
+
   return (
     <div className={style.container}>
-      <ReelComp reelData={reels[index]} reels={reels} index={index} nextVideo={nextVideo} nextBackVideo={nextBackVideo}/>
+      <ReelComp
+        reelData={reels[index]}
+        reels={reels}
+        index={index}
+        nextVideo={nextVideo}
+        nextBackVideo={nextBackVideo}
+      />
     </div>
-  )
-}
+  );
+};
 
-export default Page
+// **Wrap Page in Suspense Boundary**
+const Page = () => (
+  <Suspense fallback={<div>Loading...</div>}>
+    <ReelPage />
+  </Suspense>
+);
 
-
+export default Page;
